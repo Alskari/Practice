d34c6903c87d9bf3d5a67d97d39acae77d3a888b
Alskari <richard.david.wilson@gmail.com>
2020-08-07 17:19:15 -0500

    BET

--- Exercises_Lvl_12/main.go
+++ Exercises_Lvl_12/main.go
@@ -5,6 +5,7 @@ import (
 	//"woofwoof"
 	//"github.com/GoesToEleven/go-programming/code_samples/008-ninja-level-twelve/01/dog"
 	"github.com/Alskari/Practice/Exercises_Lvl_12/woofwoof"
+	"github.com/GoesToEleven/go-programming/code_samples/010-ninja-level-thirteen/01/finished/dog"
 )
 
 // canine
@@ -17,4 +18,5 @@ func main() {
 	fido := canine{"Hank", woofwoof.Years(7)}
 
 	fmt.Println(fido)
+	fmt.Println(dog.YearsTwo(8))
 }
